3aaddbdd94400e8e9f8ac2c2f16d77ebdeac4d70
possiblemeatball <mwalker@possiblemeatball.net>
2024-04-01 19:49:59 -0400

    remove package-lock.json

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ const count = ref(0)
   <h1 class="font-semibold text-5xl text-center py-6">{{ msg }}</h1>
   <hr>
 
-  <div class="container mx-auto text-center my-4 p-4 bg-neutral-400 dark:bg-stone-900 w-1/2">
+  <div class="container mx-auto text-center my-4 p-4 bg-neutral-400 dark:bg-stone-900 w-1/3">
     <button class="rounded px-8 py-2 mx-auto bg-natural-300 text-stone-800 dark:bg-stone-700 dark:text-neutral-300" type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
