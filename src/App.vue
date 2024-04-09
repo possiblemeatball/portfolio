2220776b68b0132a7ddf49114a110ca5c0dce22a
possiblemeatball <mwalker@possiblemeatball.net>
2024-04-09 14:36:20 -0400

    setting up for Dashboard.vue widgets

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,17 @@ import {defineAsyncComponent} from "vue";
 
 const Dashboard = defineAsyncComponent(() => import('./components/dashboard/Dashboard.vue'))
 const Portfolio = defineAsyncComponent(() => import('./components/portfolio/Portfolio.vue'))
+
+const active = Portfolio
 </script>
 
 <template>
-  <div class="bg-neutral-200 text-stone-900 dark:bg-stone-800 dark:text-neutral-100 overflow-x-hidden">
+  <div class="bg-neutral-200 text-stone-900 dark:bg-stone-800 dark:text-neutral-100 overflow-x-hidden relative">
     <Suspense>
       <Dashboard />
     </Suspense>
     <Suspense>
-      <Portfolio />
+      <Component :is="active" />
     </Suspense>
   </div>
 </template>
--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -8,17 +8,29 @@ const TitleWebGL = defineAsyncComponent(() =>
 
 <template>
   <div class=
-           "w-screen h-screen
-            grid justify-center place-content-center
+           "w-screen h-screen relative
             bg-neutral-50 dark:bg-stone-950"
   >
-    <div>
+    <div class="absolute text-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
       <div class="animate-slide-top">
         <Suspense>
           <TitleWebGL/>
         </Suspense>
       </div>
-      <div class="animate-slide-bottom text-center">meatball</div>
+      <div class="animate-slide-bottom">meatball</div>
+    </div>
+    <div class=
+             "w-screen h-screen p-4
+              grid grid-cols-2 gap-96 place-content-around z-20">
+      <div v-for="n in 4"
+           class=
+               "p-2 px-4
+              rounded-lg bg-neutral-200 dark:bg-stone-800
+              shadow shadow-neutral-300 dark:shadow-stone-800"
+      >
+        <h2 class="font-semibold">test</h2>
+        <h3>{test</h3>
+      </div>
     </div>
   </div>
 </template>
