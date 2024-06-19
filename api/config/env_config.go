7d924f751cb443599e5ff351ad2359305593d0df
possiblemeatball <meatball@manthrowshat.net>
2024-06-19 14:16:03 -0400

    api: initial fiber + gorm implementation (very bare)

---
+++ api/config/env_config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"os"
+	"strconv"
+	"time"
+)
+
+func EnvToFiber() fiber.Config {
+	prefork, _ := strconv.ParseBool(os.Getenv("FIBER_PREFORK"))
+	caseSensitive, _ := strconv.ParseBool(os.Getenv("FIBER_CASE_SENSITIVE"))
+	strictRouting, _ := strconv.ParseBool(os.Getenv("FIBER_STRICT_ROUTING"))
+	readTimeout, _ := strconv.ParseInt(os.Getenv("FIBER_READ_TIMEOUT"), 10, 64)
+
+	return fiber.Config{
+		AppName:       "portfolio api",
+		ServerHeader:  "your mother",
+		Prefork:       prefork,
+		CaseSensitive: caseSensitive,
+		StrictRouting: strictRouting,
+		ReadTimeout:   time.Second * time.Duration(readTimeout),
+	}
+}
---
+++ api/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v3/log"
+	_ "github.com/joho/godotenv/autoload"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"possiblemeatball.com/api/v2/config"
+)
+
+type Name struct {
+	gorm.Model
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+func main() {
+	appConfig := config.EnvToFiber()
+	app := fiber.New(appConfig)
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DSN:                       "gorm:gorm@tcp(localhost:3306)/gorm?charset=utf8mb4&parseTime=True&loc=Local",
+		DefaultStringSize:         64,
+		DisableDatetimePrecision:  true,
+		DontSupportRenameIndex:    true,
+		DontSupportRenameColumn:   true,
+		SkipInitializeWithVersion: false,
+	}), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	migrateErr := db.AutoMigrate(&Name{})
+	if migrateErr != nil {
+		return
+	}
+
+	db.Create(&Name{FirstName: "John", LastName: "Doe"})
+
+	api := app.Group("/api/v2", func(c *fiber.Ctx) error {
+		c.Set("Version", "2.0.0")
+		return c.Next()
+	})
+
+	api.Post("/ballsack", func(c *fiber.Ctx) error {
+		var john Name
+		db.First(&john, "first_name = ?", "John")
+		db.Model(&john).Update("LastName", "Deere")
+		return c.JSON(&john)
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
