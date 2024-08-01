9b5a09e2b1f38a0832b33591c8920f25482734bc
possiblemeatball <meatball@manthrowshat.net>
2024-08-01 11:14:49 -0400

    change tag and TEBlob color

--- components/blobs/TEBlob.vue
+++ components/blobs/TEBlob.vue
@@ -6,9 +6,9 @@ const blob = props.blob
 
 <template>
 <div class="bg-neutral-300 border-neutral-400 text-neutral-800
-            dark:bg-neutral-700 dark:border-neutral-800 dark:text-neutral-200
+            dark:bg-neutral-600 dark:border-neutral-700 dark:text-neutral-200
             flex flex-row justify-center items-center text-center gap-1
-            rounded-md border px-2  select-none pointer-events-none">
+            rounded-md border px-2 select-none pointer-events-none">
   {{ blob.skill }}
 </div>
 </template>
