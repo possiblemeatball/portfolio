32696252fa0ce4e4eed314e3df99d32c8dd30b9d
possiblemeatball <meatball@manthrowshat.net>
2024-08-08 09:32:16 -0400

    update projects to be more concise and provide urls for previous defunct projects for more information

--- components/Projects.vue
+++ components/Projects.vue
@@ -3,31 +3,37 @@ import ProjectBlob from "./blobs/ProjectBlob.vue";
 
 const blobs = [
   {
-    title: "ManThrowsHat.net",
-    url: "https://manthrowshat.net",
-    description: "Personal website that I have a little bit of fun on. I make blog posts here, as well as accept tips if you're feeling generous. Built using Golang with Fiber, TailwindCSS, and HTMX. Currently under heavy development."
+    title: "NeverTrustLLC.com",
+    url: "https://nevertrustllc.com",
+    description: "Corporate front-page website and customer portal. "
   },
   {
     title: "Rock Eaters",
     url: "https://rockeaters.net",
-    description: "Online services provider including e-mail, file upload, and search engine. Built using React, TailwindCSS, Postfix, Dovecot, and SearXNG. Currently under heavy development.",
+    description: "Online services provider including e-mail, file upload, and search engine. Currently under heavy development.",
+  },
+  {
+    title: "ManThrowsHat.net",
+    url: "https://manthrowshat.net",
+    description: "Personal website that I have a little bit of fun on. I make blog posts here, as well as accept tips if you're feeling generous."
   },
   {
     title: "Free Discord Bot",
-    url: "https://github.com/possiblemeatball/freediscordbot",
+    url: "https://manthrowshat.net/projects/freediscordbot",
     description: "Free and open source Discord Bot with a front-end web control panel. Currently in initial development stages.",
     tags: ['in-dev']
   },
   {
     title: "Hot Grabba",
+    url: "https://manthrowshat.net/projects/hot-grabba",
     description: "Android and iOS companion app for Amazon Flex drivers. Assisted drivers in meeting goals and expectations of their gig economy work. Defunct as of 2024.",
     tags: ['defunct']
   },
   {
     title: "XIV",
-    url: null,
-    description: "An event-driven Minecraft client modification. Heavy LWJGL and OpenGL usage for game render modifications. For legal and copyright reasons, source code is available upon request, with certain redactions.",
-    tags: ['abandoned', 'defunct']
+    url: "https://manthrowshat.net/projects/xiv",
+    description: "An event-driven Minecraft client modification. For legal and copyright reasons, source code is available upon request, with certain redactions.",
+    tags: ['abandoned', 'legacy']
   },
 ]
 </script>
