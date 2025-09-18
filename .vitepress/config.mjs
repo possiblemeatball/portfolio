1bb8cb3188cd5ca6aa3f9830b4f6989d60611168
possiblemeatball <meatball@manthrowshat.net>
2025-09-17 22:53:02 -0400

    Update tailwindcss v4 config

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vitepress'
+import {defineConfig} from 'vitepress'
+import Tailwind from '@tailwindcss/vite'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  lang: "en-US",
-  title: "Matthew \"possiblemeatball\" Walker",
-  description: "Software Engineer, Systems Administrator, Entrepreneur",
-  cleanUrls: true,
+    lang: "en-US",
+    title: "Matthew \"possiblemeatball\" Walker",
+    description: "Software Engineer, Systems Administrator, Entrepreneur",
+    cleanUrls: true,
+    srcExclude: ["README.md"],
+    vite: {
+        plugins: [Tailwind()]
+    }
 })
