4497cae8d013f3929743bddbb92947e054f0b312
possiblemeatball <meatball@manthrowshat.net>
2025-11-13 13:06:16 -0500

    change font from Red Hat to Ubuntu

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -19,18 +19,17 @@ const rendererSettings = {
 
 <template>
   <div class="flex flex-col gap-0 justify-center items-center overflow-x-hidden">
-    <div class="p-4 pt-20 w-screen flex flex-col justify-center items-center text-center
-                select-none pointer-events-none bg-white dark:bg-neutral-900">
+    <div class="p-4 pt-20 w-screen flex flex-col justify-center items-center text-center select-none pointer-events-none bg-white dark:bg-neutral-900">
       <div class="animate-slide-top w-[511px] h-[128px]">
         <TresCanvas v-bind="rendererSettings">
           <suspense><MeatballCanvas/></suspense>
         </TresCanvas>
       </div>
-      <h1 class="font-display text-2xl">{{ site.title }}</h1>
-      <p class="font-sans italic text-neutral-600 dark:text-neutral-400">{{ site.description }}</p>
+      <h1 class="font-medium text-2xl">{{ site.title }}</h1>
+      <p class="italic text-neutral-600 dark:text-neutral-400">{{ site.description }}</p>
     </div>
     <div class="container max-w-screen-sm p-4 text-justify">
-      <div class="prose prose-h3:font-display prose-p:font-sans max-w-none prose-neutral dark:prose-invert">
+      <div class="prose max-w-none prose-neutral dark:prose-invert">
         <p>Over 12 years experience in software engineering, systems administration, computer networking, cybersecurity,
           and general information technology, 6 years experience with client communications, customer service and
           support technician work, and 5 years of entrepreneurship.</p>
@@ -45,9 +44,8 @@ const rendererSettings = {
         <h3>Education</h3>
         <div class="p-2 flex flex-col gap-2">
           <span class="text-left">
-            <a class="font-display font-bold text-lg" href="https://miamioh.edu"
-               target="_blank">Miami University Oxford</a>
-            <span class="ml-2 font-sans font-normal">Oxford, OH</span>
+            <a class="font-bold text-lg" href="https://miamioh.edu" target="_blank">Miami University Oxford</a>
+            <span class="ml-2">Oxford, OH</span>
             <span class="ml-2 text-sm text-neutral-600 dark:text-neutral-400">2018 &mdash; 2020</span>
           </span>
           <span class="font-mono indent-8 text-sm text-neutral-600 dark:text-neutral-400">Associate's degree, maj. Information Technology min. Media & Culture, Music Theory</span>
