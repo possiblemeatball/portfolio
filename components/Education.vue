d5c0dd57e11e762db68ee8d6b1ecf4c7abfefad3
possiblemeatball <meatball@manthrowshat.net>
2025-11-24 16:47:09 -0500

    improve education ... ... to include certificates and accept objects from a list

--- components/Education.vue
+++ components/Education.vue
@@ -1,17 +1,33 @@
 <script setup>
-
+const certs = [
+    {
+        organization: "Miami University Oxford",
+        url: "https://miamioh.edu",
+        location: "Oxford, OH",
+        start: 2018,
+        end: 2020,
+        description: "Associate's degree, major in Information Technology, minor in Media & Culture, Music Theory"
+    }
+]
 </script>
 
 <template>
-    <h3>Education</h3>
-    <div class="p-2 flex flex-col gap-2">
-        <span class="text-left">
-            <a class="font-bold text-lg" href="https://miamioh.edu" target="_blank">Miami University Oxford</a>
-            <span class="ml-2">Oxford, OH</span>
-            <span class="ml-2 text-sm text-neutral-600 dark:text-neutral-400">2018 &mdash; 2020</span>
-        </span>
-        <span class="font-mono indent-8 text-sm text-neutral-600 dark:text-neutral-400">Associate's degree, maj.
-            Information Technology min. Media & Culture, Music Theory</span>
+    <h3>Education & Certifications</h3>
+    <div class="flex flex-col not-prose">
+        <div v-for="cert in certs" class="p-2 flex flex-col gap-2">
+            <span class="text-left">
+                <a class="underline font-bold text-lg" :href="cert.url" target="_blank">{{ cert.organization }}</a>
+                <span class="ml-2">{{ cert.location }}</span>
+                <span class="ml-2 text-sm text-neutral-600 dark:text-neutral-400">{{ cert.start }}
+                    <span v-if="cert.end && (cert.end !== cert.start)">&mdash; {{ cert.end }}</span>
+                    <span v-else-if="cert.end"></span>
+                    <span v-else>&mdash; Present</span>
+                </span>
+            </span>
+            <span class="font-mono indent-8 text-sm text-neutral-600 dark:text-neutral-400">
+                {{ cert.description }}
+            </span>
+        </div>
     </div>
 </template>
 
